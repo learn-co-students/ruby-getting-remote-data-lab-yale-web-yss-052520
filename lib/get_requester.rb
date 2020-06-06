742a8adf717fe0d75ca11499f76691a810722a19
edensongjbs <edensongjbs@gmail.com>
2020-06-06 13:57:46 -0400

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,11 +4,12 @@ require 'open-uri'
 require 'json'
 
 class GetRequester
+    attr_reader :url
     def initialize(url)
         @url = url
     end
     def get_response_body
-        uri = URI.parse(URL)
+        uri = URI.parse(url)
         response = Net::HTTP.get_response(uri)
         response.body
     end
